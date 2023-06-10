ce540aa21d903b4e6c65ae27e1e2e05611a59cbf
HM1 <mrtoscano94@gmail.com>
2023-06-10 13:14:12 -0600

    Ecription and decriprion working

--- index.js
+++ index.js
@@ -15,21 +15,32 @@ const copyButton = document.querySelector("#copyBtn");
 // * Event listeners
 encryptButton.addEventListener("click", (e) => encryptMessage(e));
 decryptButton.addEventListener("click", (e) => decryptMessage(e));
+copyButton.addEventListener("click", (e) => copyyEncryptedMessage(e));
 
 function toggleOutputMessage() {
-	if ( outputMessage.style.display === "none" && copyButton.style.display === "none" ) {
+	if (
+		outputMessage.style.display === "none" &&
+		copyButton.style.display === "none"
+	) {
+		munecoImg.style.display = "none";
+		noMsgTxt.style.display = "none";
+		infoMsgTxt.style.display = "none";
 		outputMessage.style.display = "block";
 		copyButton.style.display = "block";
 		return;
 	}
+	munecoImg.style.display = "block";
+	noMsgTxt.style.display = "block";
+	infoMsgTxt.style.display = "block";
 	outputMessage.style.display = "none";
 	copyButton.style.display = "none";
 }
 
 function encryptMessage(e) {
 	e.preventDefault();
+	if (inputTextarea.value.trim().length === 0) return;
 	const speratedValues = inputTextarea.value.split("");
-	const someExample = speratedValues.map((letter, index) => {
+	const encriptedWord = speratedValues.map((letter, index) => {
 		switch (letter) {
 			case "a":
 				return "ai";
@@ -51,9 +62,24 @@ function encryptMessage(e) {
 				break;
 		}
 	});
+	// * Check if the message is empty
 	toggleOutputMessage();
-	console.log(someExample.join(""));
-	console.log("Encrypting message...");
+	outputMessage.value = encriptedWord.join("");
+	// Clean input
+	// inputTextarea.value = "";
+	
+}
+
+function copyyEncryptedMessage(e) {
+	e.preventDefault();
+	navigator.clipboard
+		.writeText(outputMessage.value)
+		.then(() => {
+			// console.log("Copied to clipboard successfully!");
+		})
+		.catch((err) => {
+			throw err;
+		});
 }
 
 const reversedReplacements = {};
@@ -66,48 +92,53 @@ const replacements = {
 };
 
 function swapReplacementKeys() {
-	Object.keys(replacements).forEach((key) => reversedReplacements[replacements[key]] = key );
+	Object.keys(replacements).forEach(
+		(key) => (reversedReplacements[replacements[key]] = key)
+	);
 }
+
 swapReplacementKeys();
 toggleOutputMessage();
 
 // * All above working
 
-
-
 function decryptMessage(e) {
 	e.preventDefault();
-	const speratedValues = inputTextarea.value.trim().split(" ");
-	const separator = speratedValues.map((word) => {
-		return decriptWord(word);
-	});
-
-	console.log(separator, "separator ------- FINAL");
+	let messageToDecrypt;
+	// * No message to decrypt
+	if(inputTextarea.value.length === 0 && outputMessage.value.trim().length === 0 ) return;
+	// * Message to decrypt is inputText area
+	if (outputMessage.value.trim().length === 0 && inputTextarea.value.length !== 0) messageToDecrypt = inputTextarea.value.trim();
+	// * Message to decrypt is output area
+	else messageToDecrypt = inputTextarea.value.trim().split(" ");
+	console.log(inputTextarea.value.length, "inputTextarea.value.length");
+	let decrypted = messageToDecrypt.map((word) => decriptWord(word));
+	console.log(decrypted.join(' '), "DECRIPTED MESSAGE ******");
 }
 
+
+
 function decriptWord(word) {
+	// console.warn(word, "word");
+	// console.log(replacements, "replacements");
 	const replacementValues = Object.values(replacements);
 	let newWord;
 	//  * use includes to check if the need to be decrypted
 	replacementValues.forEach((replacement, indexfor) => {
-		if (word.includes(replacementValues[indexfor])) {
-			newWord = word.replace(replacement, reversedReplacements[replacement]);
+		
+		console.log(replacement, "replacement");
+		if (word.includes(replacement)) {
+			while (word.includes(replacement)) {
+				word = word.replace(replacement, reversedReplacements[replacement]);
+			}
 		}
+		newWord = word;
 	});
 	return newWord;
 }
-// Check recursively if a word contains coincidences
+
 function containsCoincidences(word, coincidences) {
-	// Base case: if word is empty, return false
 	if (word.length === 0) return false;
-	
-
-	// Check if the first character of the word is a coincidence
-	if (coincidences.includes(word[0])) {
-		return true;
-	}
-
-	// Recursive case: check the remaining characters
+	if (coincidences.includes(word[0])) return true;
 	return containsCoincidences(word.slice(1), coincidences);
 }
-
